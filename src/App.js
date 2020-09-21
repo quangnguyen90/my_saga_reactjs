4de47bac2048cdafc057be9f06b5309bf682c0fd
Quang Nguyen Phu <quang.nguyenphu@grevo.net>
2020-09-21 09:15:57 +0700

    1.1 - Example: Material-UI Button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
     <div className="App">
-      Hello Redux Saga
+      <h1>Hello Redux Saga</h1>
+      <Button variant="contained" color="primary">
+        Test Material UI
+      </Button>
     </div>
   );
 }
