3dd38066fcfec43ea1017c12363d57255f0682c4
Quang Nguyen Phu <quang.nguyenphu@grevo.net>
2020-09-21 10:59:55 +0700

    4.1- Build UI - Using Grid to make layout

--- src/containers/TaskBoard/index.js
+++ src/containers/TaskBoard/index.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
-import { withStyles } from '@material-ui/core';
+import { withStyles, Button } from '@material-ui/core';
 import styles from './styles';
+import AddIcon from '@material-ui/icons/Add';
+import Grid from '@material-ui/core/Grid';
+import { STATUSES } from '../../contants';
 
 class TaskBoard extends Component {
+    renderBoard() {
+        let xhtml = null;
+        xhtml = (
+            <Grid container spacing={2}>
+                {
+                    STATUSES.map((status, index) => {
+                        return (
+                            <Grid item md={4} xs={3} key={status.value}>
+                                {status.label}
+                            </Grid>
+                        )
+                    })
+                }
+            </Grid>
+        );
+        return xhtml;
+    }
     render() {
         var {classes} = this.props;
         return (
             <div className={classes.taskboard}>
-                <div className={classes.shape}>Frontend</div>
-                <div className={classes.shape}>Backend</div>
+                <Button variant="contained" color="primary" className={classes.button}>
+                    <AddIcon /> Add New Task
+                </Button>
+                {this.renderBoard()}
             </div>
         );
     }
