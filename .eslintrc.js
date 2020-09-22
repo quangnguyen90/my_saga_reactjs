c0fabb8d177f9abcbc5c2d032dfa5394e351d927
Quang Nguyen Phu <quang.nguyenphu@grevo.net>
2020-09-22 12:47:57 +0700

    6.1- Eslint Rule - P1 - Add Config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,29 @@
 module.exports = {
     parser: "babel-eslint",
-    "rules": {
-        "semi": 1,
-        "quotes": [2, "single"]
+    parserOptions: {
+        ecmaVersion: 6,
+        sourceType: "module",
+        ecmaFeatures: {
+            jsx: true
+        }
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "airbnb"
+    ],
+    rules: {
+        semi: 1,
+        quotes: [2, 'single'],
+        'react/prop-types': 1,
+        'react/jsx-max-props-per-line' : 1,
+        'linebreak-style': 0,
+        'import/no-extraneous-dependencies': 0,
+        'class-methods-use-this': 0,
+        'react/jsx-filename-extension': 0,
+        'react/jsx-one-expression-per-line': 0,
+        'react/forbid-prop-types': 0,
+        'react/require-default-props': 0,
+        'react/jsx-indent': 0,
     }
 }
