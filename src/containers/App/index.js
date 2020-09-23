35942857725bc43cc15e3481fd2fcdda4ab01044
Quang Nguyen Phu <quang.nguyenphu@grevo.net>
2020-09-23 17:26:06 +0700

    11- Integrate Redux & ReduxDevTools new version

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,16 +1,22 @@
-import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/core/styles';
-import styles from './styles';
-import TaskBoard from '../TaskBoard';
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
 import theme from '../../commons/Theme';
+import configureStore from '../../redux/configureStore';
+import TaskBoard from '../TaskBoard';
+import styles from './styles';
+
+const store = configureStore();
 
 class App extends Component {
   render() {
     return (
-      <ThemeProvider theme={theme}>
-        <TaskBoard />
-      </ThemeProvider>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <TaskBoard />
+        </ThemeProvider>
+      </Provider>
     );
   }
 }
