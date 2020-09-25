a5a00dfa1d4b87b103665536e3508483fc150789
Quang Nguyen Phu <quang.nguyenphu@grevo.net>
2020-09-25 16:43:09 +0700

    17- Install Redux-Saga

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,8 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from '../sagas';
 
 const composeEnhancers =
   process.env.NODE_ENV !== 'production' &&
@@ -11,10 +13,16 @@ const composeEnhancers =
       })
     : compose;
 
+// create the saga middleware
+const sagaMiddleware = createSagaMiddleware();
+
 const configureStore = () => {
-  const middlewares = [thunk];
+  const middlewares = [thunk, sagaMiddleware];
   const enhancers = [applyMiddleware(...middlewares)];
+  // mount rootReducer & middleware to store
   const store = createStore(rootReducer, composeEnhancers(...enhancers));
+  // run sageMiddleware
+  sagaMiddleware.run(rootSaga);
   return store;
 };
 
